97247e8f62c679c7984fc3cdb195b595c80a015f
Student <example@example.com>
2019-05-21 09:55:38 -0700

    add buttons lunch dinner

--- UnSugarCodedCS/Controllers/LogBooksController.cs
+++ UnSugarCodedCS/Controllers/LogBooksController.cs
@@ -38,6 +38,35 @@ public ActionResult Create(string food, string sugarLevel, DateTime stampTime, s
 	List<LogBook> newList = LogBook.GetAll();
 	return View("Index",newList);
 }
+
+[HttpPost("/lunch")]
+public ActionResult CreateLunch(string foodLunch, string sugarLevelLunch, DateTime stampTimeLunch, string carbLunch)
+{
+	LogBook newLogBook = new LogBook(foodLunch, "", "", "", stampTimeLunch, new DateTime(), new DateTime(), new DateTime(), float.Parse(sugarLevelLunch), 0, 0, 0, float.Parse(carbLunch), 0, 0, 0, 5);
+	newLogBook.Save();
+	List<LogBook> newList = LogBook.GetAll();
+	return View("Index",newList);
+}
+
+[HttpPost("/dinner")]
+public ActionResult CreateDinner(string foodDinner, string sugarLevelDinner, DateTime stampTimeDinner, string carbDinner)
+{
+	LogBook newLogBook = new LogBook(foodDinner, "", "", "", stampTimeDinner, new DateTime(), new DateTime(), new DateTime(), float.Parse(sugarLevelDinner), 0, 0, 0, float.Parse(carbDinner), 0, 0, 0, 5);
+	newLogBook.Save();
+	List<LogBook> newList = LogBook.GetAll();
+	return View("Index",newList);
+}
+
+[HttpPost("/snack")]
+public ActionResult CreateSnack(string foodSnack, string sugarLevelSnack, DateTime stampTimeSnack, string carbSnack)
+{
+	LogBook newLogBook = new LogBook(foodSnack, "", "", "", stampTimeSnack, new DateTime(), new DateTime(), new DateTime(), float.Parse(sugarLevelSnack), 0, 0, 0, float.Parse(carbSnack), 0, 0, 0, 5);
+	newLogBook.Save();
+	List<LogBook> newList = LogBook.GetAll();
+	return View("Index",newList);
+}
+
+
 [HttpGet("/LogBooks/indexlogbook")]
 public ActionResult AllLogBooks()
 {
